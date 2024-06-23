4480bf87e7d46dfbdd1f3f0e5044c1317b914160
Adwait251998 <kulkarniadwait1@gmail.com>
2024-06-23 15:16:29 +0530

    Changes reverted

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import headerLogo from '../assets/investment-calculator-logo.png'
-export default function Header(){
-    return(
-        <div id='header'>
-            <img src={headerLogo} alt='Page Header'></img>
-            <h1 id="">Investment Calculator</h1>
-        </div>
-    )
-}
